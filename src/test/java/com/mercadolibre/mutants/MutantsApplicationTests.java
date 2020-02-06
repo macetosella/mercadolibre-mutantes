fac3437c979a8f29e6a9a315c14400f751734c0b
Mati <acetosella90@gmail.com>
2020-02-06 10:27:21 -0300

    feat: The structure of the project was created
    The project was created with Spring Boot and the Spring Web dependency was added

---
+++ src/test/java/com/mercadolibre/mutants/MutantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.mutants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
