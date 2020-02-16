7ef80c8b1b7bc300907d6b2d0f1ee417e4e44d4c
Matias Acetosella <macetosella@est.frba.utn.edu.ar>
2020-02-16 16:06:30 -0300

    test: add MutantApplication run test

--- src/test/java/com/mercadolibre/mutants/MutantsApplicationTests.java
+++ src/test/java/com/mercadolibre/mutants/MutantsApplicationTests.java
@@ -3,12 +3,18 @@ package com.mercadolibre.mutants;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.ActiveProfiles;
 
-
+@ActiveProfiles("test")
 @DataMongoTest(excludeAutoConfiguration = {EmbeddedMongoAutoConfiguration.class})
 class MutantsApplicationTests extends MutantsApplication{
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void mutantsApplication(){
+		MutantsApplication.main(new String[]{});
 	}
 }
